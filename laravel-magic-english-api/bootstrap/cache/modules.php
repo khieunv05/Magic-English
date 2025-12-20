592cd96c06defbf6683121a4d123f2d63491e055
hoanglan <hoanglando15@gmail.com>
2025-12-20 08:28:16 +0700

    feat: add base api

---
+++ laravel-magic-english-api/bootstrap/cache/modules.php
@@ -0,0 +1,15 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Admin\\Providers\\AdminServiceProvider',
+    1 => 'Modules\\User\\Providers\\UserServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Admin\\Providers\\AdminServiceProvider',
+    1 => 'Modules\\User\\Providers\\UserServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
---
+++ laravel-magic-english-api/bootstrap/cache/packages.php
@@ -0,0 +1,102 @@
+<?php return array (
+  'intervention/image-laravel' => 
+  array (
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Laravel\\Facades\\Image',
+    ),
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'laravel/pail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Pail\\PailServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/socialite' => 
+  array (
+    'aliases' => 
+    array (
+      'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
+    ),
+    'providers' => 
+    array (
+      0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'nwidart/laravel-modules' => 
+  array (
+    'aliases' => 
+    array (
+      'Module' => 'Nwidart\\Modules\\Facades\\Module',
+    ),
+    'providers' => 
+    array (
+      0 => 'Nwidart\\Modules\\LaravelModulesServiceProvider',
+    ),
+  ),
+  'pestphp/pest-plugin-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Pest\\Laravel\\PestServiceProvider',
+    ),
+  ),
+  'treconyl/images-upload' => 
+  array (
+    'aliases' => 
+    array (
+      'ImageUpload' => 'Treconyl\\ImagesUpload\\Facades\\ImageUpload',
+    ),
+    'providers' => 
+    array (
+      0 => 'Treconyl\\ImagesUpload\\ImagesUploadServiceProvider',
+    ),
+  ),
+);
