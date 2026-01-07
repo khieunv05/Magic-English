854cf9cfcf059e69e3f731c6b8736131dafe69f0
unknown <khieu.nv05@gmail.com>
2026-01-07 20:31:13 +0700

    Pull magic vocab to test

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
-
+  //https://844e1eac2b60.ngrok-free.app
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://844e1eac2b60.ngrok-free.app',
+    defaultValue: 'https://kristal-nondichotomous-conclusionally.ngrok-free.dev',
   );
 }
